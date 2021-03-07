04c0b38af2e8adfd655d05b3b274d182ae4ffd1d
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-03-06 19:20:06 -0500

    proyectos laravel

--- lawp_laravel/routes/web.php
+++ lawp_laravel/routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::resource('attachments',\App\Http\Controllers\AttachmentController::class);
