b6484994bdb1d99703d8a4ad27970b2f1cac27ee
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-03-18 10:22:48 -0500

    tyutyu

--- lawp_laravel/app/Models/user.php
+++ lawp_laravel/app/Models/user.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 
 class user extends Model
 {
-    use HasFactory, hasApiTokens, Notifiable;
+    use HasFactory;
     protected $table = 'users';
     protected $primaryKey = 'id';
     protected $fillable = [
