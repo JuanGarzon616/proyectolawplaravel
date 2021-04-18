90bd119a9e1a24cc9a7a893d5cfaff893d81ea1e
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-04-18 15:32:59 -0500

    sdfgdsfg

---
+++ lawp_laravel/database/migrations/2021_02_23_200830_create_pqr_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePqrTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pqr_types', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pqr_types');
+    }
+}
