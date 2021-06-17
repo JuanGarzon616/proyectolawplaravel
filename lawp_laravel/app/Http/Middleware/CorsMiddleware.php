8fdf6f8dca2419f531821c2c3edcf06481422f7b
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-06-17 11:03:24 -0500

    cors

--- lawp_laravel/app/Http/Middleware/CorsMiddleware.php
+++ lawp_laravel/app/Http/Middleware/CorsMiddleware.php
@@ -18,7 +18,7 @@ class CorsMiddleware
     {
         return $next($request)
             //Url a la que se le dará acceso en las peticiones
-            ->header("Access-Control-Allow-Origin", "http://urlfronted.example")
+            ->header("Access-Control-Allow-Origin", "http://localhost:8080")
             //Métodos que a los que se da acceso
             ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
             //Headers de la petición
--- lawp_laravel/routes/api.php
+++ lawp_laravel/routes/api.php
@@ -3,44 +3,47 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api as api;
 
+Route::group(['middleware' => ['cors']], function () {
+    Route::post('/user/login/', [api\UserControllerApi::class, 'authenticate']);
+    Route::post('/user/register/', [api\UserControllerApi::class, 'register']);
 
-Route::post('/user/login/', [api\UserControllerApi::class, 'authenticate']);
-Route::post('/user/register/', [api\UserControllerApi::class, 'register']);
+    Route::group(['middleware' => ['jwt.verify']], function () {
 
-Route::group(['middleware' => ['jwt.verify']], function () {
+        Route::put('/user/{id}', [api\UserControllerApi::class, 'update']);
+        Route::delete('/user/{id}', [api\UserControllerApi::class, 'destroy']);
+        Route::get('/user/', [api\UserControllerApi::class, 'getAuthenticatedUser']);
+        Route::post('/response/', [api\ResponseControlleApi::class, 'store']);
+        Route::get('/response/{id}', [api\ResponseControlleApi::class, 'show']);
+        Route::get('/response/', [api\ResponseControlleApi::class, 'index']);
+        Route::get('/getbusi/', [api\BusinessControllerApi::class, 'selectBusi']);
 
-    Route::put('/user/{id}', [api\UserControllerApi::class, 'update']);
-    Route::delete('/user/{id}', [api\UserControllerApi::class, 'destroy']);
-    Route::get('/user/', [api\UserControllerApi::class, 'getAuthenticatedUser']);
-    Route::post('/response/', [api\ResponseControlleApi::class, 'store']);
-    Route::get('/response/{id}', [api\ResponseControlleApi::class, 'show']);
-    Route::get('/response/', [api\ResponseControlleApi::class, 'index']);
-    Route::get('/getbusi/', [api\BusinessControllerApi::class, 'selectBusi']);
+        Route::group(['middleware' => ['is_normal_user']], function () {
+            Route::post('/pqr/', [api\PqrControllerApi::class, 'store']);
+            Route::post('/pqr/affair/', [api\PqrControllerApi::class, 'selectUserAffair']);
+            Route::post('/business/register/', [api\BusinessControllerApi::class, 'register']);
+            Route::get('/pqr/user/{id}', [api\PqrControllerApi::class, 'showForUser']);
+            Route::put('/pqr/{id}', [api\PqrControllerApi::class, 'update']);
+            Route::delete('/pqr/{id}', [api\PqrControllerApi::class, 'destroy']);
+        });
 
-    Route::group(['middleware' => ['is_normal_user']], function () {
-        Route::post('/pqr/', [api\PqrControllerApi::class, 'store']);
-        Route::post('/pqr/affair/', [api\PqrControllerApi::class, 'selectUserAffair']);
-        Route::post('/business/register/', [api\BusinessControllerApi::class, 'register']);
-        Route::get('/pqr/user/{id}', [api\PqrControllerApi::class, 'showForUser']);
-        Route::put('/pqr/{id}', [api\PqrControllerApi::class, 'update']);
-        Route::delete('/pqr/{id}', [api\PqrControllerApi::class, 'destroy']);
+        Route::group(['middleware' => ['is_admin']], function () {
+            Route::get('/users/', [api\UserControllerApi::class, 'index']);
+            Route::get('/business/', [api\BusinessControllerApi::class, 'index']);
+        });
+
+        Route::group(['middleware' => ['is_business']], function () {
+            Route::get('/business/{id}', [api\BusinessControllerApi::class, 'show']);
+            Route::get('/pqr/business/{id}', [api\PqrControllerApi::class, 'showForBus']);
+            Route::put('/business/{id}', [api\BusinessControllerApi::class, 'update']);
+            Route::delete('/business/{id}', [api\BusinessControllerApi::class, 'destroy']);
+
+        });
     });
 
-    Route::group(['middleware' => ['is_admin']], function () {
-        Route::get('/users/', [api\UserControllerApi::class, 'index']);
-        Route::get('/business/', [api\BusinessControllerApi::class, 'index']);
-    });
+    Route::get('/departament/', [api\DepartamentControllerApi::class, 'index'])->name('departament.index');
+    Route::get('/departament/{id}', [api\DepartamentControllerApi::class, 'show'])->name('departament.show');
+    Route::get('/economicsectors/', [api\DepartamentControllerApi::class, 'economic']);
 
-    Route::group(['middleware' => ['is_business']], function () {
-        Route::get('/business/{id}', [api\BusinessControllerApi::class, 'show']);
-        Route::get('/pqr/business/{id}', [api\PqrControllerApi::class, 'showForBus']);
-        Route::put('/business/{id}', [api\BusinessControllerApi::class, 'update']);
-        Route::delete('/business/{id}', [api\BusinessControllerApi::class, 'destroy']);
 
-    });
 });
-
-Route::get('/departament/', [api\DepartamentControllerApi::class, 'index'])->name('departament.index');
-Route::get('/departament/{id}', [api\DepartamentControllerApi::class, 'show'])->name('departament.show');
-Route::get('/economicsectors/',[api\DepartamentControllerApi::class, 'economic']);
 
